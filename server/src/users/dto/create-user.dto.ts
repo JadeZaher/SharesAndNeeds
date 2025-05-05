21684cb876fc444b55218c3169bca57fea2a8ba8
Jade Zaher <82454482+JadeZaher@users.noreply.github.com>
2025-05-05 16:57:25 -0600

    Add server/src/users/dto/create-user.dto.ts 1746485843840

---
+++ server/src/users/dto/create-user.dto.ts
@@ -0,0 +1,17 @@
+import { IsEmail, IsString, MinLength, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail({}, { message: 'Please provide a valid email address' })
+  @IsNotEmpty({ message: 'Email is required' })
+  email: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Username is required' })
+  @MinLength(3, { message: 'Username must be at least 3 characters long' })
+  username: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Password is required' })
+  @MinLength(6, { message: 'Password must be at least 6 characters long' })
+  password: string;
+}
